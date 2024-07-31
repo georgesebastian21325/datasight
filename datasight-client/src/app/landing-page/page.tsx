af4de13114fc9c1172d76eac54bb59c957f106ba
Escolario <georgesebastian21325@gmail.com>
2024-07-31 13:23:42 +0800

    Configured prettier

---
+++ datasight-client/src/app/landing-page/page.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function page() {
+  return (
+    <main className="font-sans">
+      <div className="text-center flex flex-col items-center ">
+        <h1 className="font-bold text-6xl tracking-tighter">
+          Translating Data into Valuable Insights.
+        </h1>
+        <p className="max-w-[700px] text-primary-foreground/80 md:text-xl ">
+          Designed to transform complex data into actionable insights, our tool
+          provides a seamless, intuitive interface for visualizing and aiding
+          your decisions.
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default page;
