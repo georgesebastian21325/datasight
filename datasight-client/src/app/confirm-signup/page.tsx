d7eedd67921f6c365000c980d7be002db96d0141
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-16 15:17:15 +0800

    added confirm-sign-up form and page

---
+++ datasight-client/src/app/confirm-signup/page.tsx
@@ -0,0 +1,5 @@
+import ConfirmSignUpForm from "../forms/confirm-sign-up-form";
+
+export default function LoginPage() {
+	return <ConfirmSignUpForm />;
+}
--- datasight-client/src/app/sign-up/page.tsx
+++ datasight-client/src/app/sign-up/page.tsx
@@ -1,49 +1,56 @@
 "use client";
 
 import React, { useState } from "react";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import {
+	AiOutlineEye,
+	AiOutlineEyeInvisible,
+} from "react-icons/ai";
 import CompanyLogo from "../../assets/company-logo.jpg";
 import Image from "next/image";
 import { LoginBtn } from "../components/button";
+import SignUpForm from "../forms/sign-up-form";
 
 export default function Page() {
-  const [selectedRole, setSelectedRole] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
-  const isSignUpPage = true;
+	const [selectedRole, setSelectedRole] = useState("");
+	const [showPassword, setShowPassword] = useState(false);
+	const [showConfirmPassword, setShowConfirmPassword] =
+		useState(false);
+	const isSignUpPage = true;
 
-  const handleRoleChange = (event) => {
-    setSelectedRole(event.target.value);
-  };
+	const handleRoleChange = (event: any) => {
+		setSelectedRole(event.target.value);
+	};
 
-  const togglePasswordVisibility = () => {
-    setShowPassword((prev) => !prev);
-  };
+	const togglePasswordVisibility = () => {
+		setShowPassword((prev) => !prev);
+	};
 
-  const toggleConfirmPasswordVisibility = () => {
-    setShowConfirmPassword((prev) => !prev);
-  };
+	const toggleConfirmPasswordVisibility = () => {
+		setShowConfirmPassword((prev) => !prev);
+	};
 
-  return (
-    <div className="font-sans grid grid-cols-2 h-screen">
-      <div className="flex flex-col justify-center items-center bg-gradient-to-br from-black to-brand-blue rounded-tr-2xl rounded-br-2xl">
-        <div className="flex flex-col items-center justify-center text-center">
-          <h1 className="gradient-text font-bold text-4xl">
-            See Data Differently with Datasight.
-          </h1>
-          <p className="text-white">
-            Unlock powerful insights and make better decisions with our tool.
-          </p>
+	return (
+		<div className="font-sans grid grid-cols-2 h-screen">
+			<div className="flex flex-col justify-center items-center bg-gradient-to-br from-black to-brand-blue rounded-tr-2xl rounded-br-2xl">
+				<div className="flex flex-col items-center justify-center text-center">
+					<h1 className="gradient-text font-bold text-4xl">
+						See Data Differently with Datasight.
+					</h1>
+					<p className="text-white">
+						Unlock powerful insights and make better
+						decisions with our tool.
+					</p>
 
-          <h1 className="text-white mt-12">
-            Already have an account? Login in now.
-          </h1>
-          <LoginBtn isSignUpPage={isSignUpPage} />
-        </div>
-      </div>
-      <div className="flex items-center justify-center">
-        <div className="w-full max-w-md bg-white shadow-lg rounded-lg p-5 border-2">
-          <div className="text-center flex flex-col items-center">
+					<h1 className="text-white mt-12">
+						Already have an account? Login in now.
+					</h1>
+					<LoginBtn isSignUpPage={isSignUpPage} />
+				</div>
+			</div>
+			<div className="flex items-center justify-center">
+				<div className="w-full max-w-md bg-white shadow-lg rounded-lg p-5 border-2">
+					<SignUpForm />
+					{/* <div className="text-center flex flex-col items-center">
             <Image src={CompanyLogo} alt="Company Logo" height={50} />
             <p className="gradient-text font-bold text-2xl">
               Create your account
@@ -187,9 +194,9 @@ export default function Page() {
             <button className="w-full bg-black text-white py-2 px-4 rounded-md">
               Sign Up
             </button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+          </div> */}
+				</div>
+			</div>
+		</div>
+	);
 }
