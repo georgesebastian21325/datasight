ad1586733a3ff37dd66482eb081b73b82db3ac4a
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2024-08-17 14:51:49 +0800

    updated bg color of login and sign up form

--- datasight-client/src/app/forms/login-form.tsx
+++ datasight-client/src/app/forms/login-form.tsx
@@ -21,7 +21,7 @@ export default function LoginForm() {
 			action={dispatch}
 			className="space-y-3"
 		>
-			<div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+			<div className="flex-1 rounded-lg  px-6 pb-4 pt-8">
 				<h1 className={` mb-3 text-2xl`}>
 					Please log in to continue.
 				</h1>
--- datasight-client/src/app/forms/sign-up-form.tsx
+++ datasight-client/src/app/forms/sign-up-form.tsx
@@ -23,7 +23,7 @@ export default function SignUpForm() {
 			action={dispatch}
 			className="space-y-3"
 		>
-			<div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+			<div className="flex-1 rounded-lg px-6 pb-4 pt-8">
 				<h1 className={` mb-3 text-2xl`}>
 					Please create an account.
 				</h1>
