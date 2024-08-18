cc33a78f4380c1a9303368686e556f5e72e1bc3b
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-08-18 23:09:08 +0800

    adjusted margins for btn placeholder

--- datasight-client/src/app/home-page/page.tsx
+++ datasight-client/src/app/home-page/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
                 <div className='flex items-center justify-center'>
                     <Image src={CompanyLogo} alt='company logo' />
                 </div>
-                <div className='bg-brand-blue py-[1rem] px-[15rem] rounded-md mt-12'>
+                <div className='bg-brand-blue py-[1rem] px-[12rem] rounded-md mt-12'>
                     <div className='flex flex-col items-center justify-center text-center text-white '>
                         <h1 className='font-semibold text-xl mb-5'> Welcome John Doe, how can we help you today? </h1>
                         <p> Manage your enterprise architecture with ease. </p>
