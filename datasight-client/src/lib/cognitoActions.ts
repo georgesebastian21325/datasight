2ae0d0f6736a6fcb45836e92fba726616f21888a
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-09-30 15:51:03 +0800

    code refactoring for Authentication feature

--- datasight-client/src/lib/cognitoActions.ts
+++ datasight-client/src/lib/cognitoActions.ts
@@ -6,10 +6,6 @@ import {
   signOut,
   resendSignUpCode,
   autoSignIn,
-  updateUserAttribute,
-  type UpdateUserAttributeOutput,
-  confirmUserAttribute,
-  updatePassword,
   resetPassword,
   confirmResetPassword,
 } from "aws-amplify/auth";
@@ -81,7 +77,7 @@ export async function handleSignIn(
   prevState: string | undefined,
   formData: FormData
 ) {
-  let redirectLink = "/dashboard";
+  let redirectLink = "/home-page";
   try {
     const { isSignedIn, nextStep } = await signIn({
       username: String(formData.get("email")),
@@ -109,100 +105,6 @@ export async function handleSignOut() {
   redirect("/auth/login");
 }
 
-export async function handleUpdateUserAttribute(
-  prevState: string,
-  formData: FormData
-) {
-  let attributeKey = "name";
-  let attributeValue;
-  let currentAttributeValue;
-
-  if (formData.get("email")) {
-    attributeKey = "email";
-    attributeValue = formData.get("email");
-    currentAttributeValue = formData.get("current_email");
-  } else {
-    attributeValue = formData.get("name");
-    currentAttributeValue = formData.get("current_name");
-  }
-
-  if (attributeValue === currentAttributeValue) {
-    return "";
-  }
-
-  try {
-    const output = await updateUserAttribute({
-      userAttribute: {
-        attributeKey: String(attributeKey),
-        value: String(attributeValue),
-      },
-    });
-    return handleUpdateUserAttributeNextSteps(output);
-  } catch (error) {
-    console.log(error);
-    return "error";
-  }
-}
-
-function handleUpdateUserAttributeNextSteps(output: UpdateUserAttributeOutput) {
-  const { nextStep } = output;
-
-  switch (nextStep.updateAttributeStep) {
-    case "CONFIRM_ATTRIBUTE_WITH_CODE":
-      const codeDeliveryDetails = nextStep.codeDeliveryDetails;
-      return `Confirmation code was sent to ${codeDeliveryDetails?.deliveryMedium}.`;
-    case "DONE":
-      return "success";
-  }
-}
-
-export async function handleUpdatePassword(
-  prevState: "success" | "error" | undefined,
-  formData: FormData
-) {
-  const currentPassword = formData.get("current_password");
-  const newPassword = formData.get("new_password");
-
-  if (currentPassword === newPassword) {
-    return;
-  }
-
-  try {
-    await updatePassword({
-      oldPassword: String(currentPassword),
-      newPassword: String(newPassword),
-    });
-  } catch (error) {
-    console.log(error);
-    return "error";
-  }
-
-  return "success";
-}
-
-export async function handleConfirmUserAttribute(
-  prevState: "success" | "error" | undefined,
-  formData: FormData
-) {
-  const code = formData.get("code");
-
-  if (!code) {
-    return;
-  }
-
-  try {
-    await confirmUserAttribute({
-      userAttributeKey: "email",
-      confirmationCode: String(code),
-    });
-  } catch (error) {
-    console.log(error);
-    return "error";
-  }
-
-  return "success";
-}
-
 export async function handleResetPassword(
   prevState: string | undefined,
   formData: FormData
@@ -228,5 +130,5 @@ export async function handleConfirmResetPassword(
   } catch (error) {
     return getErrorMessage(error);
   }
-  redirect("/login");
+  redirect("/");
 }
