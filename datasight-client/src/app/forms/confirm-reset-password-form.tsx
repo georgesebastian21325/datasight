62777cf3184edd9e1902f1bf881bda3f954f6649
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-10-28 12:56:02 +0800

    Added background image for reset password

--- datasight-client/src/app/forms/confirm-reset-password-form.tsx
+++ datasight-client/src/app/forms/confirm-reset-password-form.tsx
@@ -5,103 +5,157 @@ import {
     ExclamationCircleIcon,
     KeyIcon,
 } from "@heroicons/react/24/outline";
-import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { EyeIcon, EyeSlashIcon, ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "../components/button";
 import { useFormState, useFormStatus } from "react-dom";
 import { handleConfirmResetPassword } from "@/lib/cognitoActions";
+import CompanyLogo from '../../assets/company-logo.jpg'
+import BackgroundImage from '../../assets/background-image.jpg'
+import Image from "next/image";
+import { useState } from "react";
 
 export default function ConfirmResetPasswordForm() {
     const [errorMessage, dispatch] = useFormState(
         handleConfirmResetPassword,
         undefined
     );
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+
     return (
-        <form action={dispatch} className="flex flex-col items-center justify-center space-y-3 mt-[10rem]">
-            <div className="flex flex-col rounded-lg w-[30%] px-6 pb-4 pt-8 items-center border-2">
-                <h1 className='font-bold gradient-text text-xl mb-1'>
-                    CONFIRMATION CODE SENT.
-                </h1>
-                <h1 className={`mb-4 text-sm text-gray-500`}>
-                    Kindly view your confirmation code via email. 
-                </h1>
-                <div className="w-full">
-                    <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="email"
-                        >
-                            Email
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="email"
-                                type="email"
-                                name="email"
-                                placeholder="Enter your email address"
-                                required
-                            />
-                            <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+        <div
+            className="min-h-screen bg-center bg-fixed "
+            style={{ backgroundImage: `url(${BackgroundImage.src})`  }}
+        >
+            <form action={dispatch} className="flex flex-col items-center justify-center  space-y-3  p-10 rounded-lg">
+                <div className="flex flex-col rounded-lg w-[30%] px-6 pb-4 pt-8 items-center border-2 mt-10">
+                    <Image src={CompanyLogo} alt='company_logo' width={170} height={50} />
+                    <h1 className='font-bold gradient-text text-xl mb-1'>
+                        CONFIRMATION CODE SENT.
+                    </h1>
+                    <h1 className={`mb-4 text-sm text-gray-500`}>
+                        Kindly view your confirmation code via email.
+                    </h1>
+                    <div className="w-full">
+                        <div>
+                            <label
+                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                htmlFor="email"
+                            >
+                                Email
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    placeholder="Enter your email address"
+                                    required
+                                />
+                                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <label
+                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                htmlFor="password"
+                            >
+                                New Password
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                    id="password"
+                                    type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    placeholder="Enter password"
+                                    required
+                                    minLength={6}
+                                />
+                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                <div
+                                    onClick={togglePasswordVisibility}
+                                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
+                                >
+                                    {showPassword ? (
+                                        <EyeSlashIcon className="h-5 w-5" />
+                                    ) : (
+                                        <EyeIcon className="h-5 w-5" />
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <label
+                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                htmlFor="confirmPassword"
+                            >
+                                Confirm Password
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                    id="confirmPassword"
+                                    type={showPassword ? "text" : "password"}
+                                    name="confirmPassword"
+                                    placeholder="Confirm your password"
+                                    required
+                                    minLength={6}
+                                />
+                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                <div
+                                    onClick={togglePasswordVisibility}
+                                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-500"
+                                >
+                                    {showPassword ? (
+                                        <EyeSlashIcon className="h-5 w-5" />
+                                    ) : (
+                                        <EyeIcon className="h-5 w-5" />
+                                    )}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="mt-4">
+                            <label
+                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                htmlFor="code"
+                            >
+                                Confirmation Code
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                    id="code"
+                                    type="text"
+                                    name="code"
+                                    placeholder="Enter code"
+                                    required
+                                    minLength={6}
+                                />
+                                <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
                         </div>
                     </div>
-                    <div className="mt-4">
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="password"
+                    <ResetPasswordButton />
+                    <div className="flex h-8 items-end space-x-1">
+                        <div
+                            className="flex h-8 items-end space-x-1"
+                            aria-live="polite"
+                            aria-atomic="true"
                         >
-                            New Password
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="password"
-                                type="password"
-                                name="password"
-                                placeholder="Enter password"
-                                required
-                                minLength={6}
-                            />
-                            <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-                        </div>
-                    </div>
-                    <div className="mt-4">
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="code"
-                        >
-                            Confirmation Code
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="code"
-                                type="text"
-                                name="code"
-                                placeholder="Enter code"
-                                required
-                                minLength={6}
-                            />
-                            <KeyIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            {errorMessage && (
+                                <>
+                                    <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                                    <p className="text-sm text-red-500">{errorMessage}</p>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
-                <ResetPasswordButton />
-                <div className="flex h-8 items-end space-x-1">
-                    <div
-                        className="flex h-8 items-end space-x-1"
-                        aria-live="polite"
-                        aria-atomic="true"
-                    >
-                        {errorMessage && (
-                            <>
-                                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                                <p className="text-sm text-red-500">{errorMessage}</p>
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 }
 
--- datasight-client/src/app/forms/submit-reset-password-form.tsx
+++ datasight-client/src/app/forms/submit-reset-password-form.tsx
@@ -12,57 +12,64 @@ import { Button } from "../components/button";
 import { useFormState, useFormStatus } from "react-dom";
 import { handleResetPassword } from "@/lib/cognitoActions";
 
+import BackgroundImage from '../../assets/background-image.jpg'
+
 export default function SubmitResetPasswordFrom() {
     const [errorMessage, dispatch] = useFormState(handleResetPassword, undefined);
     return (
-        <form action={dispatch} className="flex flex-col items-center justify-center space-y-3 mt-[10rem]">
-            <div className="flex flex-col rounded-lg w-[30%] px-6 pb-4 pt-8 items-center border-2">
-                <Image src={CompanyLogo} alt='company_logo' width={170} height={50} />
-                <h1 className='font-bold gradient-text text-xl '> RESET PASSWORD </h1>
-                <h1 className={`mb-3 text-sm text-gray-500`}>
-                    Please enter your email to get confirmation code.
-                </h1>
-                <div className="w-[70%]">
-                    <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="email"
+        <div
+            className="min-h-screen bg-cover bg-center bg-fixed "
+            style={{ backgroundImage: `url(${BackgroundImage.src})` }}
+        >
+            <form action={dispatch} className="flex flex-col items-center justify-center space-y-3">
+                <div className="flex flex-col rounded-lg w-[30%] px-6 pb-4 pt-8 items-center border-2 mt-[12rem]">
+                    <Image src={CompanyLogo} alt='company_logo' width={170} height={50} />
+                    <h1 className='font-bold gradient-text text-xl '> RESET PASSWORD </h1>
+                    <h1 className={`mb-3 text-sm text-gray-500`}>
+                        Please enter your email to get the confirmation code.
+                    </h1>
+                    <div className="w-[70%]">
+                        <div>
+                            <label
+                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                htmlFor="email"
+                            >
+                                Email
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    placeholder="Enter your email address"
+                                    required
+                                />
+                                <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex space-x-5 w-[70%]'>
+                        <BackBtn />
+                        <SendConfirmationCodeButton />
+                    </div>
+                    <div className="flex h-8 items-end space-x-1">
+                        <div
+                            className="flex h-8 items-end space-x-1"
+                            aria-live="polite"
+                            aria-atomic="true"
                         >
-                            Email
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="email"
-                                type="email"
-                                name="email"
-                                placeholder="Enter your email address"
-                                required
-                            />
-                            <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            {errorMessage && (
+                                <>
+                                    <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+                                    <p className="text-sm text-red-500">{errorMessage}</p>
+                                </>
+                            )}
                         </div>
                     </div>
                 </div>
-                <div className='flex space-x-5 w-[70%]'>
-                    <BackBtn />
-                    <SendConfirmationCodeButton />
-                </div>
-                <div className="flex h-8 items-end space-x-1">
-                    <div
-                        className="flex h-8 items-end space-x-1"
-                        aria-live="polite"
-                        aria-atomic="true"
-                    >
-                        {errorMessage && (
-                            <>
-                                <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
-                                <p className="text-sm text-red-500">{errorMessage}</p>
-                            </>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 }
 
