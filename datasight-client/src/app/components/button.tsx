faebf87dab6fb119de9ac4aabec0eb2a14d83443
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-10-31 12:15:30 +0800

    added checkAttributes api

--- datasight-client/src/app/components/button.tsx
+++ datasight-client/src/app/components/button.tsx
@@ -7,61 +7,6 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import { useFormState, useFormStatus } from "react-dom";
 import { useState } from 'react'
 
-function LandingPageSignUpBtn() {
-	return (
-		<Link
-			href="auth/sign-up"
-			className="bg-brand-blue px-5 rounded-md py-[0.5rem] mt-[-0.1rem] hover:bg-blue-800 h-50 transform transition-transform duration-300 hover:scale-105"
-		>
-			Sign Up
-		</Link>
-	);
-}
-
-function LoginBtn({
-	isSignUpPage = false,
-	isLoginPage = false,
-}) {
-	return (
-		<Link
-			href="auth/login"
-			className={`rounded-md mt-2 hover:bg-gray-700 transform transition-transform duration-300 hover:scale-105 ${isSignUpPage
-				? "text-black bg-white font-bold hover:bg-black px-5 py-2 hover:text-white"
-				: "px-5 bg-black text-white  py-2 flex mt-[-0.1rem]"
-				} ${isLoginPage ? "hidden" : ""}`}
-		>
-			Login To Your Account
-		</Link>
-	);
-}
-
-function SignUpBtn() {
-	const { pending } = useFormStatus();
-
-	return (
-		<Button
-			className="mt-4 w-full flex justify-center items-center bg-brand-blue"
-			aria-disabled={pending}
-		>
-			<span className="flex items-center">
-				Create Acount
-				<ArrowRightIcon className="ml-2 h-5 w-5 text-gray-50" />
-			</span>
-		</Button>
-	);
-}
-
-
-function UploadDataSetBtn() {
-	return (
-		<Link
-			href='./upload-dataset'
-			className='bg-white text-black text-sm px-12 font-semibold py-3 rounded-md hover:bg-black hover:text-white transform transition-transform duration-300 hover:scale-105'
-		>
-			UPLOAD DATASET
-		</Link>
-	)
-}
 
 function EnterpriseArchitectureBtn() {
 	return (
