f9a9a60f101e0d8a9476a45daf20921afdd08060
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-11-02 21:42:40 +0800

    added product-dashboard

---
+++ datasight-client/src/app/dashboard/products/page.tsx
@@ -0,0 +1,12 @@
+import ProductDashboard from '../../../vcomponents/dashboard-ui/product-dashboard'
+import NavigationBar from '../../components/global/NavigationBar'
+
+
+export default function page() {
+    return (
+        <>
+            <NavigationBar />
+            <ProductDashboard />
+        </>
+    )
+}
