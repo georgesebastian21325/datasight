97d2608d828489ce7040b04cba0aca55c0c2589c
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-11-03 20:54:04 +0800

    added responsiveness of home-page

--- datasight-client/src/vcomponents/home-page.tsx
+++ datasight-client/src/vcomponents/home-page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from "react";
+import Link from "next/link";
 import { Button } from "@/vcomponents/home-page-components/button";
 import { useRouter } from 'next/navigation';
 import NavigationBar from '../app/components/global/NavigationBar';
@@ -32,8 +33,8 @@ export default function HomePage() {
       {isLoading ? (
         <LoadingPage /> // Show LoadingPage while loading
       ) : (
-        <main>
-          <NavigationBar />
+        <main className='relative z-10 flex flex-col text-center'>
+            <NavigationBar />
           <section className="py-24">
             <div className="container mx-auto px-4">
               <h1 className="text-4xl font-bold mb-8 text-center bg-clip-text text-transparent bg-gradient-to-r from-[#1050d2] to-[#f47820]">
@@ -42,47 +43,56 @@ export default function HomePage() {
               <p className="text-xl text-center mb-12 text-gray-600">
                 Transform your insights and empower the future with our tool.
               </p>
-
-              {/* Navigation Buttons */}
-              <div className="grid grid-cols-4 gap-4 max-w-4xl mx-auto text-center">
-                <Button
-                  onClick={() => navigateWithLoading('/dashboard/resources', 'Loading Resource Dashboard...')}
-                  className="w-full hover:bg-gray-800 hover:shadow-xl"
-                  size="lg"
-                >
-                  Resource Dashboard
-                </Button>
-                <Button
-                  onClick={() => navigateWithLoading('/dashboard/products', 'Loading Product Dashboard...')}
-                  className="w-full hover:bg-gray-800 hover:shadow-xl"
-                  size="lg"
-                >
-                  Product Dashboard
-                </Button>
-                <Button
-                  onClick={() => navigateWithLoading('/dashboard/services', 'Loading Service Dashboard...')}
-                  className="w-full hover:bg-gray-800 hover:shadow-xl"
-                  size="lg"
-                >
-                  Service Dashboard
-                </Button>
-                <Button
-                  onClick={() => navigateWithLoading('/dashboard/offerings', 'Loading Offering Dashboard...')}
-                  className="w-full hover:bg-gray-800 hover:shadow-xl"
-                  size="lg"
-                >
-                  Offering Dashboard
-                </Button>
-              </div>
-
-              <div className="flex justify-center mt-5">
-                <Button
-                  onClick={() => navigateWithLoading('/enterprise-architecture-view', 'Loading Enterprise Architecture...')}
-                  className="w-full max-w-[15rem] bg-brand-blue hover:bg-blue-800 hover:shadow-xl"
-                  size="lg"
-                >
-                  Enterprise Architecture
-                </Button>
+              <div className='w-full max-w-4xl mx-auto space-y-4'>
+                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3">
+                  <Link href='/dashboard/resources' >
+                    <Button
+                      onClick={() => navigateWithLoading('/dashboard/resources', 'Loading Resource Dashboard...')}
+                      className="md:w-full w-[40%] hover:bg-gray-800 hover:shadow-xl "
+                      size="lg"
+                    >
+                      Resource Dashboard
+                    </Button>
+                  </Link>
+                  <Link href='/dashboard/products' >
+                    <Button
+                      onClick={() => navigateWithLoading('/dashboard/products', 'Loading Product Dashboard...')}
+                      className="md:w-full hover:bg-gray-800 hover:shadow-xl"
+                      size="lg"
+                    >
+                      Product Dashboard
+                    </Button>
+                  </Link>
+                  <Link href='/dashboard/services' >
+                    <Button
+                      onClick={() => navigateWithLoading('/dashboard/services', 'Loading Service Dashboard...')}
+                      className="md:w-full hover:bg-gray-800 hover:shadow-xl"
+                      size="lg"
+                    >
+                      Service Dashboard
+                    </Button>
+                  </Link>
+                  <Link href='/dashboard/offerings' >
+                    <Button
+                      onClick={() => navigateWithLoading('/dashboard/offerings', 'Loading Offering Dashboard...')}
+                      className="md:w-full hover:bg-gray-800 hover:shadow-xl"
+                      size="lg"
+                    >
+                      Offering Dashboard
+                    </Button>
+                  </Link>
+                </div>
+                  <div className='flex justify-center'>
+                    <Link href='/dashboard/enterprise-architecture-view' >
+                      <Button
+                        onClick={() => navigateWithLoading('/enterprise-architecture-view', 'Loading Enterprise Architecture...')}
+                        className="md:w-full w-[90%] text-[0.9rem] bg-brand-blue hover:bg-blue-800"
+                        size="lg"
+                      >
+                        Enterprise Architecture
+                      </Button>
+                    </Link>
+                  </div>
               </div>
             </div>
           </section>
