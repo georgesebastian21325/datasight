7c072bc7c4799c43215bb249f59fc864cfd13fed
George Sebastian <130885164+georgesebastian21325@users.noreply.github.com>
2024-11-22 04:16:54 +0800

    fixed offering ui

--- datasight-client/src/vcomponents/dashboard-ui/offering-dashboard.tsx
+++ datasight-client/src/vcomponents/dashboard-ui/offering-dashboard.tsx
@@ -162,7 +162,7 @@ export default function OfferingDashboardComponent() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-4"> 
         <Card> 
           <CardHeader>
-            <CardTitle> Offering Revenue Forecast</CardTitle>
+            <CardTitle className="text-xl font-bold"> Offering Revenue Forecast</CardTitle>
           </CardHeader>
           <CardContent>
               <OfferingRevenueForecastChart data={offeringRevenueForecast} />
